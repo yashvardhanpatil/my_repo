a5ec69a4e25ab803f0af1931cff90594773f6a75
yashvardhanpatil <patilyashvardhan551@gmail.com>
2024-12-26 13:17:03 +0530

    new_ver

--- web.py
+++ web.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-data = { "name": ['John','Alice','Bob','Mary'],
-        "age" : [23,45,32,34]
+data = { "name": ['John','Alice','Bob','Mary','yash'],
+        "age" : [23,45,32,34,24]
 }
 
 df = pd.DataFrame(data)
